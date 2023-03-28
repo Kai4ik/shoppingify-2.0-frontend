80329f3d829d01ea703de1a46a537feb45ba5399
Kai <orozobekov.kai@gmail.com>
2023-03-28 03:29:12 -0400

    ci: solving ts error -  any in conditional

--- shoppingify-2.0-frontend/middleware.ts
+++ shoppingify-2.0-frontend/middleware.ts
@@ -1,30 +1,32 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { verifyUser } from "./utils/auth";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { verifyUser } from './utils/auth'
+import { RequestCookie } from 'next/dist/server/web/spec-extension/cookies'
 
-export default async function middleware(
+export default async function middleware (
   req: NextRequest
 ): Promise<NextResponse> {
-  const cookies = req.cookies.getAll();
-  let idToken = "";
-  cookies.forEach((cookie) => {
-    if (Boolean(cookie.name.includes("idToken"))) idToken = cookie.value;
-  });
+  const cookies = req.cookies.getAll()
+  let idToken = ''
+  cookies.forEach((cookie: RequestCookie) => {
+    const ifIdTokenCookie: boolean = cookie.name.includes('idToken')
+    if (ifIdTokenCookie) idToken = cookie.value
+  })
 
   if (idToken.length === 0) {
-    req.nextUrl.pathname = "/login";
-    return NextResponse.redirect(req.nextUrl);
+    req.nextUrl.pathname = '/login'
+    return NextResponse.redirect(req.nextUrl)
   }
 
-  const ifLoggedIn = await verifyUser(idToken);
-  if (Boolean(ifLoggedIn.succcess)) {
-    return NextResponse.next();
+  const ifLoggedIn = await verifyUser(idToken)
+  if (ifLoggedIn.succcess) {
+    return NextResponse.next()
   }
 
-  req.nextUrl.pathname = "/login";
-  return NextResponse.redirect(req.nextUrl);
+  req.nextUrl.pathname = '/login'
+  return NextResponse.redirect(req.nextUrl)
 }
 
 export const config = {
-  matcher: "/protected/:path*",
-};
+  matcher: '/protected/:path*'
+}
