5a3f18556584b98946504be8e34e6966bcbc0918
Kai <orozobekov.kai@gmail.com>
2023-04-19 17:01:53 -0400

    refactor: tooltip mobile issue fix

--- shoppingify-2.0-frontend/app/protected/tooltip.tsx
+++ shoppingify-2.0-frontend/app/protected/tooltip.tsx
@@ -1,44 +1,45 @@
-'use client'
+"use client";
 
 // external modules
-import { Tooltip, WrapItem, Icon } from '@chakra-ui/react'
-import { IconType } from 'react-icons/lib'
-import NextLink from 'next/link'
+import { Tooltip, WrapItem, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons/lib";
+import NextLink from "next/link";
 
 interface Props {
-  label: string
-  size: number
-  link: string
-  icon: IconType
+  label: string;
+  size: number;
+  link: string;
+  icon: IconType;
 }
 
-export default function TooltipCn ({
+export default function TooltipCn({
   label,
   link,
   size,
-  icon
+  icon,
 }: Props): JSX.Element {
   return (
     <Tooltip
       label={label}
       aria-label={label}
-      placement='right'
+      placement="right"
       gutter={18}
       hasArrow
-      bg='main'
-      color='secondary'
+      bg="main"
+      color="secondary"
+      closeOnScroll={true}
     >
       <WrapItem>
         <NextLink href={link}>
           <Icon
             as={icon}
             boxSize={size}
-            _hover={{ color: 'secondary', boxSize: size + 1 }}
-            cursor='pointer'
-            color='main'
+            _hover={{ color: "secondary", boxSize: size + 1 }}
+            cursor="pointer"
+            color="main"
           />
         </NextLink>
       </WrapItem>
     </Tooltip>
-  )
+  );
 }
